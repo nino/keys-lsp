ffa57cf570640aa09adfc15803dfa3f311d19a62
Nino Annighoefer <nino@ninoan.com>
2023-05-27 09:15:21 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,31 @@ struct Backend {
 #[tower_lsp::async_trait]
 impl LanguageServer for Backend {
     async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
-        Ok(InitializeResult::default())
+        let mut res = InitializeResult::default();
+        res.capabilities.hover_provider = Some(HoverProviderCapability::Simple(true));
+        Ok(res)
     }
 
     async fn initialized(&self, _: InitializedParams) {
         self.client
             .log_message(MessageType::INFO, "server initialized!")
             .await;
+    }
+
+    async fn did_change(&self, _: DidChangeTextDocumentParams) {
+        self.client
+            .log_message(MessageType::INFO, "file changed!")
+            .await;
+    }
+
+    async fn hover(&self, _params: HoverParams) -> Result<Option<Hover>> {
+        self.client
+            .log_message(MessageType::INFO, "Hovering!")
+            .await;
+        Ok(Some(Hover {
+            contents: HoverContents::Scalar(MarkedString::String("Hello World!".to_string())),
+            range: None,
+        }))
     }
 
     async fn shutdown(&self) -> Result<()> {
